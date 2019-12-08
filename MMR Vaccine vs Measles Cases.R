53c770ca0c3207222dbd40d6a0454b5f1d1f99b1
tiffanguyen <tiffanguyen@chapman.edu>
2019-12-08 02:56:21 -0800

    Finished All possible graphs, now just needs to edit colors to fit in presentation

--- MMR Vaccine vs Measles Cases.R
+++ MMR Vaccine vs Measles Cases.R
@@ -1,2 +1,10 @@
 setwd("~/Desktop/R Scripts/Preventable-Diseases")
-vacconerns <- read.csv()
+comp <- read.csv("MMRvsCases.csv")
+comp1 <- comp[19942:19975,]
+
+ratio <- comp1[,5]/comp1[,4]
+
+library(ggplot2)
+c<- ggplot(comp1, aes(x= Year, y= ratio)) + geom_line(color = "red")
+c + labs(title ="Measles Cases Per Year over MMR Vaccine Coverage by Year", y = "Ratio of Cases Over Vaccine Vocerage", x = "Year") + 
+  theme_minimal()
--- Vaccine Concerns Reported By Parents of Children Age 6 Or Younger, 2010.R
+++ Vaccine Concerns Reported By Parents of Children Age 6 Or Younger, 2010.R
@@ -1,2 +1,6 @@
 setwd("~/Desktop/R Scripts/Preventable-Diseases")
-vacconerns <- read.csv()
+vacconcerns <- read.csv("VacConcerns.csv")
+pie(x = vacconcerns$Parents.reporting.concern...., 
+    label=paste(vacconcerns$Concern),
+    col=rainbow(length(vacconcerns$Concern)), cex = 0.8,
+    main="Vaccine Concerns Reported By Parents, 2010") #will have to change width when exporting as JPG
